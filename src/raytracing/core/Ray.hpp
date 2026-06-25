394a7468f46ad83c5b5b479423ec3b47cea9883e
kieliszakd <duncan.kieliszak@inp-toulouse.fr>
2026-06-25 14:35:23 +0200

    feat: adapted the Ray

--- src/raytracing/core/Ray.hpp
+++ src/raytracing/core/Ray.hpp
@@ -18,5 +18,9 @@ namespace Raytracing
          * The number of bounces that the ray have done.
          */
         unsigned int bounce = 0;
+        /**
+         * The material currently containing the ray, or -1 if the ray is in air.
+         */
+        int mediumMaterialIndex = -1;
     };
 } // namespace Raytracing
